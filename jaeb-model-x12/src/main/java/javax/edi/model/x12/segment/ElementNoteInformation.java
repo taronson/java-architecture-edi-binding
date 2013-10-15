7960fa098f3d065370db14be488482691960541b
John Eckstein <jeckstei@redhat.com>
2013-10-15 16:12:23 -0400

    edi 997

--- jaeb-model-x12/src/main/java/javax/edi/model/x12/segment/ElementNoteInformation.java
+++ jaeb-model-x12/src/main/java/javax/edi/model/x12/segment/ElementNoteInformation.java
@@ -1,18 +1,69 @@
 package javax.edi.model.x12.segment;
 
+import java.math.BigInteger;
+
 import javax.edi.bind.annotations.EDIElement;
 import javax.edi.bind.annotations.EDISegment;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@EDISegment(tag="AK4")
+@EDISegment(tag = "AK4")
 public class ElementNoteInformation {
+
+	@EDIElement(fieldName = "AK401", dataElement = "C030")
+	@NotNull
+	private BigInteger positionInSegment;
+
+	@EDIElement(fieldName = "AK402", dataElement = "725")
+	@Size(min = 1, max = 4)
+	private BigInteger dataElementReferenceNumber;
+
+	@EDIElement(fieldName = "AK403", dataElement = "723")
+	@NotNull
+	@Size(min = 1, max = 3)
+	private BigInteger dataElementSyntaxErrorCode;
+
+	@EDIElement(fieldName = "AK404", dataElement = "724")
+	@Size(min = 1, max = 99)
+	private String copyOfBadDataElement;
 
 	@Override
 	public String toString() {
 		return "ElementNoteInformation []";
 	}
 
-	
+	public BigInteger getPositionInSegment() {
+		return positionInSegment;
+	}
 
-	
+	public void setPositionInSegment(BigInteger positionInSegment) {
+		this.positionInSegment = positionInSegment;
+	}
+
+	public BigInteger getDataElementReferenceNumber() {
+		return dataElementReferenceNumber;
+	}
+
+	public void setDataElementReferenceNumber(
+			BigInteger dataElementReferenceNumber) {
+		this.dataElementReferenceNumber = dataElementReferenceNumber;
+	}
+
+	public BigInteger getDataElementSyntaxErrorCode() {
+		return dataElementSyntaxErrorCode;
+	}
+
+	public void setDataElementSyntaxErrorCode(
+			BigInteger dataElementSyntaxErrorCode) {
+		this.dataElementSyntaxErrorCode = dataElementSyntaxErrorCode;
+	}
+
+	public String getCopyOfBadDataElement() {
+		return copyOfBadDataElement;
+	}
+
+	public void setCopyOfBadDataElement(String copyOfBadDataElement) {
+		this.copyOfBadDataElement = copyOfBadDataElement;
+	}
+
 }
--- jaeb-model-x12/src/main/java/javax/edi/model/x12/segment/SegmentNoteInformation.java
+++ jaeb-model-x12/src/main/java/javax/edi/model/x12/segment/SegmentNoteInformation.java
@@ -1,5 +1,7 @@
 package javax.edi.model.x12.segment;
 
+import java.math.BigInteger;
+
 import javax.edi.bind.annotations.EDIElement;
 import javax.edi.bind.annotations.EDISegment;
 import javax.validation.constraints.NotNull;
@@ -17,28 +19,51 @@ public class SegmentNoteInformation {
 	@EDIElement(fieldName="AK302", dataElement="719")
 	@NotNull
 	@Size(min=1, max=6)
-	private String segmentPositioninTransactionSet;
-
+	private BigInteger segmentPositioninTransactionSet;
+	
+	@EDIElement(fieldName="AK303", dataElement="447")
+	@Size(min=1, max=6)
+	private String loopIdentifierCode;
+	
+	@EDIElement(fieldName="AK304", dataElement="720")
+	@Size(min=1, max=3)
+	private BigInteger segmentSyntaxErrorCode;
 
 	public String getSegmentIDCode() {
 		return segmentIDCode;
 	}
 
-
 	public void setSegmentIDCode(String segmentIDCode) {
 		this.segmentIDCode = segmentIDCode;
 	}
 
-
-	public String getSegmentPositioninTransactionSet() {
+	public BigInteger getSegmentPositioninTransactionSet() {
 		return segmentPositioninTransactionSet;
 	}
 
-
 	public void setSegmentPositioninTransactionSet(
-			String segmentPositioninTransactionSet) {
+			BigInteger segmentPositioninTransactionSet) {
 		this.segmentPositioninTransactionSet = segmentPositioninTransactionSet;
 	}
+
+	public String getLoopIdentifierCode() {
+		return loopIdentifierCode;
+	}
+
+	public void setLoopIdentifierCode(String loopIdentifierCode) {
+		this.loopIdentifierCode = loopIdentifierCode;
+	}
+
+	public BigInteger getSegmentSyntaxErrorCode() {
+		return segmentSyntaxErrorCode;
+	}
+
+	public void setSegmentSyntaxErrorCode(BigInteger segmentSyntaxErrorCode) {
+		this.segmentSyntaxErrorCode = segmentSyntaxErrorCode;
+	}
+
+
+
 
 	
 }
